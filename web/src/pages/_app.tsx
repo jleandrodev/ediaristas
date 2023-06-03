e0e88293f28ea21c54597d48d6762d83f0c773e1
Ozório <jleandro.dev@gmail.com>
2023-06-03 12:45:41 -0300

    feat: Theme configurado

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material";
+import theme from "ui/themes/theme";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head></Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
