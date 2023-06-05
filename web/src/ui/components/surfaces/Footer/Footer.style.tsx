cafcc1c653fb3f34d0a45d905b18e618accb6e2c
Ozório <jleandro.dev@gmail.com>
2023-06-05 18:29:17 -0300

    feat(footer): footer finalized

--- web/src/ui/components/surfaces/Footer/Footer.style.tsx
+++ web/src/ui/components/surfaces/Footer/Footer.style.tsx
@@ -1,1 +1,30 @@
-import { styled } from "@mui/material";
+import { Container, styled } from "@mui/material";
+
+export const FooterStyled = styled("footer")`
+  background-color: ${({ theme }) => theme.palette.primary.main};
+  color: ${({ theme }) => theme.palette.primary.contrastText};
+  padding: ${({ theme }) => theme.spacing(4)} 0;
+  margin-top: auto;
+`;
+
+export const FooterContainer = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+  gap: ${({ theme }) => theme.spacing(2)};
+  flex-wrap: wrap;
+
+  ${({ theme }) => theme.breakpoints.down("md")} {
+    gap: ${({ theme }) => theme.spacing(5)};
+  }
+`;
+
+export const AppList = styled("ul")`
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  gap: ${({ theme }) => theme.spacing()};
+
+  img {
+    width: 122px;
+  }
+`;
--- web/src/ui/components/surfaces/Footer/Footer.tsx
+++ web/src/ui/components/surfaces/Footer/Footer.tsx
@@ -1,5 +1,52 @@
+import { Typography, Box } from "@mui/material";
+import { AppList, FooterContainer, FooterStyled } from "./Footer.style";
+
 const Footer: React.FC = () => {
-  return <></>;
+  return (
+    <FooterStyled>
+      <FooterContainer>
+        <Box sx={{ maxWidth: "400px" }}>
+          <Typography
+            sx={{ fontWeight: "bold" }}
+            component={"h3"}
+            variant={"body1"}
+          >
+            Quem somos
+          </Typography>
+          <Typography variant={"body2"} sx={{ mt: 2 }}>
+            O e-diaristas te ajuda a encontrar um profissional perfeito para
+            realizar a limpeza da sua casa. Garantimos os melhores profissionais
+            com total segurança e praticidade! São milhares de clientes
+            satisfeitos por todo o país.
+          </Typography>
+        </Box>
+        <div>
+          <Typography
+            sx={{ fontWeight: "bold" }}
+            component={"h3"}
+            variant={"body1"}
+          >
+            Baixe nosso aplicativo
+          </Typography>
+          <AppList>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/app-store.png"} alt={"App store logo"} />
+              </a>
+            </li>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
+                <img
+                  src={"/img/logos/google-play.png"}
+                  alt={"Play store logo"}
+                />
+              </a>
+            </li>
+          </AppList>
+        </div>
+      </FooterContainer>
+    </FooterStyled>
+  );
 };
 
 export default Footer;
