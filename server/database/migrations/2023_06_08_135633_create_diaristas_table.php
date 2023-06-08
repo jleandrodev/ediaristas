dba932f1b69346b618ecd3b486365754befee0e8
Ozório <jleandro.dev@gmail.com>
2023-06-08 11:57:04 -0300

    feat(manager interface): template inicialized

---
+++ server/database/migrations/2023_06_08_135633_create_diaristas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('diaristas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome_completo', 100);
+            $table->char('cpf', 11);
+            $table->string('email', 100);
+            $table->char('telefone', 11);
+            $table->string('logradouro');
+            $table->string('numero', 20);
+            $table->string('bairro', 50);
+            $table->string('complemento', 50)->nullable();
+            $table->char('cep', 8);
+            $table->char('estado', 2);
+            $table->integer('codigo_ibge');
+            $table->string('foto_usuario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('diaristas');
+    }
+};
